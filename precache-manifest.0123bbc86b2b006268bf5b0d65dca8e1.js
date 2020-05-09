c9580bbefc24a5d89a6bc1c0eda22682a7be7b0b
Jesus Gonzalez <jagltoro@gmail.com>
2020-05-09 17:57:03 -0300

    Updates

---
+++ precache-manifest.0123bbc86b2b006268bf5b0d65dca8e1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35ef7fd66b6b346dbb7e7ac74cb53b03",
+    "url": "/movies/index.html"
+  },
+  {
+    "revision": "d760e2c0f19e81272c10",
+    "url": "/movies/static/css/main.47e9375b.chunk.css"
+  },
+  {
+    "revision": "b974686759c20fc75f68",
+    "url": "/movies/static/js/2.eb52984e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movies/static/js/2.eb52984e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d760e2c0f19e81272c10",
+    "url": "/movies/static/js/main.09851dcf.chunk.js"
+  },
+  {
+    "revision": "c129cf4d6532ae1bbdf3",
+    "url": "/movies/static/js/runtime-main.a1387a95.js"
+  },
+  {
+    "revision": "23bf5239bc957868e12a4c8fd3457d80",
+    "url": "/movies/static/media/loader.23bf5239.svg"
+  },
+  {
+    "revision": "858518b923ff082f7da57e0aedf66f82",
+    "url": "/movies/static/media/logo.858518b9.svg"
+  }
+]);
